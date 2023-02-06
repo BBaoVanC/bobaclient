85d2834671da3e5bde8f133f859cbbe63366a71f
BBaoVanC <bbaovanc@bbaovanc.com>
2023-02-06 16:39:55 -0600

    Small cleanup

--- include/types.hpp
+++ include/types.hpp
@@ -6,6 +6,15 @@
 #include <chrono>
 #include <optional>
 
+NLOHMANN_JSON_NAMESPACE_BEGIN
+// https://json.nlohmann.me/features/arbitrary_types/#how-do-i-convert-third-party-types
+template <typename T>
+struct adl_serializer<std::optional<T>> {
+    static void to_json(nlohmann::json &j, const std::optional<T> &opt);
+    static void from_json(const nlohmann::json &j, std::optional<T> &opt);
+};
+NLOHMANN_JSON_NAMESPACE_END
+
 namespace bobaclient::types {
     struct InfoResponse {
         std::string id;
@@ -18,33 +27,7 @@ namespace bobaclient::types {
         std::optional<std::string> expiry_date;
     };
 
-    //void from_json(const nlohmann::json &j, InfoResponse &r);
-    //void from_json(const nlohmann::json &j, std::optional<std::string> &r);
+    void from_json(const nlohmann::json &j, InfoResponse &r);
 }
-
-// TODO: fix this and split it up into .cpp as it should be
-using json = nlohmann::json;
-NLOHMANN_JSON_NAMESPACE_BEGIN
-// https://json.nlohmann.me/features/arbitrary_types/#how-do-i-convert-third-party-types
-template <typename T>
-struct adl_serializer<std::optional<T>> {
-    static void to_json(json &j, const std::optional<T> &opt) {
-        if (opt == std::nullopt) {
-            j = nullptr;
-        } else {
-            j = *opt; // this will call adl_serializer<T>::to_json which will
-                      // find the free function to_json in T's namespace!
-        }
-    }
-    static void from_json(const json &j, std::optional<T> &opt) {
-        if (j.is_null()) {
-            opt = std::nullopt;
-        } else {
-            opt = j.get<T>(); // same as above, but with
-                              // adl_serializer<T>::from_json
-        }
-    }
-};
-NLOHMANN_JSON_NAMESPACE_END
 
 #endif
--- src/types.cpp
+++ src/types.cpp
@@ -2,26 +2,39 @@
 
 #include <nlohmann/json.hpp>
 
-//NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(bobaclient::types::InfoResponse, id, url, direct_url, filename, mimetype, creation_date, expiry_date)
+// TODO: get this working
+//namespace bobaclient::types {
+//    NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(bobaclient::types::InfoResponse, id, url, direct_url, filename, mimetype, creation_date, expiry_date)
+//}
 
-using json = nlohmann::json;
-
-void bobaclient::types::from_json(const json &j, bobaclient::types::InfoResponse &r) {
+void bobaclient::types::from_json(const nlohmann::json &j, bobaclient::types::InfoResponse &r) {
     j.at("id").get_to(r.id);
     j.at("url").get_to(r.url);
     j.at("direct_url").get_to(r.direct_url);
     j.at("filename").get_to(r.filename);
     j.at("mimetype").get_to(r.mimetype);
     j.at("creation_date").get_to(r.creation_date);
-    r.expiry_date = j.at("expiry_date").get<std::optional<std::string>>();
+    j.at("expiry_date").get_to(r.expiry_date);
 }
 
-/*
-void bobaclient::types::from_json(const json &j, std::optional<std::string> &r) {
-    if (j.is_null()) {
-        r = std::nullopt;
+NLOHMANN_JSON_NAMESPACE_BEGIN
+// no static: see https://stackoverflow.com/a/31305772
+template <typename T>
+void adl_serializer<std::optional<T>>::to_json(nlohmann::json &j, const std::optional<T> &opt) {
+    if (opt == std::nullopt) {
+        j = nullptr;
     } else {
-        r = j.get<std::string>();
+        j = *opt; // this will call adl_serializer<T>::to_json which will
+                  // find the free function to_json in T's namespace!
     }
 }
-*/
+template <typename T>
+void adl_serializer<std::optional<T>>::from_json(const nlohmann::json &j, std::optional<T> &opt) {
+    if (j.is_null()) {
+        opt = std::nullopt;
+    } else {
+        opt = j.get<T>(); // same as above, but with
+                          // adl_serializer<T>::from_json
+    }
+}
+NLOHMANN_JSON_NAMESPACE_END
