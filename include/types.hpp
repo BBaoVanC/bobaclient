7a399fbf933f58d932b3ebfa49bf7d6f311ccb54
BBaoVanC <bbaovanc@bbaovanc.com>
2023-02-06 12:40:20 -0600

    WIP working deserialize date to string

--- include/types.hpp
+++ include/types.hpp
@@ -13,13 +13,38 @@ namespace bobaclient::types {
         std::string direct_url;
         std::string filename;
         std::string mimetype;
-        std::chrono::time_point<std::chrono::system_clock> creation_date;
-        std::optional<std::chrono::time_point<std::chrono::system_clock>> expiry_date;
+        // ISO 8601 format
+        std::string creation_date;
+        std::optional<std::string> expiry_date;
     };
 
-    void from_json(const nlohmann::json &j, InfoResponse &r);
-    void from_json(const nlohmann::json &j, std::chrono::time_point<std::chrono::system_clock> &r);
-    void from_json(const nlohmann::json &j, std::optional<std::chrono::time_point<std::chrono::system_clock>> &r);
+    //void from_json(const nlohmann::json &j, InfoResponse &r);
+    //void from_json(const nlohmann::json &j, std::optional<std::string> &r);
 }
+
+// TODO: fix this and split it up into .cpp as it should be
+using json = nlohmann::json;
+NLOHMANN_JSON_NAMESPACE_BEGIN
+// https://json.nlohmann.me/features/arbitrary_types/#how-do-i-convert-third-party-types
+template <typename T>
+struct adl_serializer<std::optional<T>> {
+    static void to_json(json &j, const std::optional<T> &opt) {
+        if (opt == std::nullopt) {
+            j = nullptr;
+        } else {
+            j = *opt; // this will call adl_serializer<T>::to_json which will
+                      // find the free function to_json in T's namespace!
+        }
+    }
+    static void from_json(const json &j, std::optional<T> &opt) {
+        if (j.is_null()) {
+            opt = std::nullopt;
+        } else {
+            opt = j.get<T>(); // same as above, but with
+                              // adl_serializer<T>::from_json
+        }
+    }
+};
+NLOHMANN_JSON_NAMESPACE_END
 
 #endif
--- src/types.cpp
+++ src/types.cpp
@@ -4,26 +4,24 @@
 
 //NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(bobaclient::types::InfoResponse, id, url, direct_url, filename, mimetype, creation_date, expiry_date)
 
-void bobaclient::types::from_json(const nlohmann::json &j, bobaclient::types::InfoResponse &r) {
+using json = nlohmann::json;
+
+void bobaclient::types::from_json(const json &j, bobaclient::types::InfoResponse &r) {
     j.at("id").get_to(r.id);
     j.at("url").get_to(r.url);
     j.at("direct_url").get_to(r.direct_url);
     j.at("filename").get_to(r.filename);
     j.at("mimetype").get_to(r.mimetype);
-    // TODO: give these real values
-    //j.at("creation_date").get_to(r.creation_date);
-    //j.at("expiry_date").get_to(r.expiry_date);
-    //r.creation_date = 0;
-    //r.expiry_date = std::nullopt;
+    j.at("creation_date").get_to(r.creation_date);
+    r.expiry_date = j.at("expiry_date").get<std::optional<std::string>>();
 }
-void bobaclient::types::from_json(const nlohmann::json &j, std::chrono::time_point<std::chrono::system_clock> &r) {
-    std::string val = j.get<std::string>();
-    val >> std::chrono::parse("%F%z", r);
-}
-void bobaclient::types::from_json(const nlohmann::json &j, std::optional<std::chrono::time_point<std::chrono::system_clock>> &r) {
+
+/*
+void bobaclient::types::from_json(const json &j, std::optional<std::string> &r) {
     if (j.is_null()) {
         r = std::nullopt;
     } else {
-        //r = j.get();
+        r = j.get<std::string>();
     }
 }
+*/
