7f929fb9831f19c66c894da891cb6caeef8b8745
BBaoVanC <bbaovanc@bbaovanc.com>
2023-02-10 14:47:47 -0600

    WIP fix exception i think

--- include/json.hpp
+++ include/json.hpp
@@ -11,8 +11,29 @@ NLOHMANN_JSON_NAMESPACE_BEGIN
 // https://json.nlohmann.me/features/arbitrary_types/#how-do-i-convert-third-party-types
 template <typename T>
 struct adl_serializer<std::optional<T>> {
-    static void to_json(nlohmann::json &j, const std::optional<T> &opt);
-    static void from_json(const nlohmann::json &j, std::optional<T> &opt);
+    static void to_json(nlohmann::json &j, const std::optional<T> &opt) {
+        if (opt == std::nullopt) {
+            j = nullptr;
+        } else {
+            j = *opt; // this will call adl_serializer<T>::to_json which will
+                      // find the free function to_json in T's namespace!
+        }
+    }
+    static void from_json(const nlohmann::json &j, std::optional<T> &opt) {
+        if (j.is_null()) {
+            opt = std::nullopt;
+        } else {
+            opt = j.get<T>(); // same as above, but with
+                              // adl_serializer<T>::from_json
+        }
+    }
+};
+
+template <>
+struct adl_serializer<bobaclient::BobashareException> {
+    static bobaclient::BobashareException from_json(const nlohmann::json &j) {
+        return bobaclient::BobashareException(j.at("message").get<std::string>(), j.at("error"));
+    }
 };
 NLOHMANN_JSON_NAMESPACE_END
 
--- src/json.cpp
+++ src/json.cpp
@@ -1,6 +1,5 @@
 #include "json.hpp"
 
-#include "errors.hpp"
 #include "types.hpp"
 
 #include <nlohmann/json.hpp>
@@ -22,30 +21,4 @@ namespace bobaclient {
         j.at("creation_date").get_to(r.creation_date);
         j.at("expiry_date").get_to(r.expiry_date);
     }
-    void from_json(const json &j, BobashareException &e) {
-        e = BobashareException(j.at("message").get<std::string>(), j.at("error"));
-    }
 }
-
-// TODO: maybe move this into header even though it somehow magically works here
-NLOHMANN_JSON_NAMESPACE_BEGIN
-// no static: see https://stackoverflow.com/a/31305772
-template <typename T>
-void adl_serializer<std::optional<T>>::to_json(json &j, const std::optional<T> &opt) {
-    if (opt == std::nullopt) {
-        j = nullptr;
-    } else {
-        j = *opt; // this will call adl_serializer<T>::to_json which will
-                  // find the free function to_json in T's namespace!
-    }
-}
-template <typename T>
-void adl_serializer<std::optional<T>>::from_json(const json &j, std::optional<T> &opt) {
-    if (j.is_null()) {
-        opt = std::nullopt;
-    } else {
-        opt = j.get<T>(); // same as above, but with
-                          // adl_serializer<T>::from_json
-    }
-}
-NLOHMANN_JSON_NAMESPACE_END
